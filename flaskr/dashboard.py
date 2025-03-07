099a98af133c0daafdb8e1217bbea60b6907ba9c
James Zingel <j.zingel@unsw.edu.au>
2025-03-07 16:21:37 +1100

    Added sidebar

--- flaskr/dashboard.py
+++ flaskr/dashboard.py
@@ -1,10 +1,13 @@
 # HTML/CSS template renderer methods
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
 @bp.route("/")
 def dashboard():
-    return render_template("dashboard.html")
+    fridge = request.args.get('fridge', type=str)
+    pretty_fridge = "" if fridge is None else fridge.title()
+    return render_template("dashboard.html", fridge=fridge, pretty_fridge=pretty_fridge)
+
 

aaba9e7a69fc1c57082905ec23e8f5b244afbbb8
jrzingel <james@zingel.co.nz>
2025-03-22 18:32:17 +1100

    Added sensor page when clicking the toggle

--- flaskr/dashboard.py
+++ flaskr/dashboard.py
@@ -9,11 +9,14 @@ bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 @bp.route("/")
 def dashboard():
     fridge = request.args.get('fridge', type=str)
+    data_type = request.args.get('type', type=str)
+    showPressures = data_type == "pressures"
+
     if fridge is None:
         # Render all the fridges at once
         return render_template("combined_dashboard.html", title="Fridge Status", url=request.url_root)
     else:
         # Render just one fridge
-        return render_template("dashboard.html", fridge=fridge, title=f"{fridge.title()} Fridge Status", url=request.url_root)
+        return render_template("dashboard.html", fridge=fridge, title=f"{fridge.title()} Fridge Status", url=request.url_root, showPressures=showPressures)
 
 

614d18366fd704c13109278743e140ca0b0183ad
jrzingel <j.zingel@unsw.edu.au>
2025-04-24 13:55:32 +1000

    ADjust threshold for oil alert

--- alarm/alerts/HotOilTemp.py
+++ alarm/alerts/HotOilTemp.py
@@ -12,28 +12,28 @@ from typing import override
 from .Alert import Alert
 
 
-# NORMAL < 30 < WARNING < 35 < ALARM
+# NORMAL < 35 < WARNING < 38 < ALARM
 
 class HotOilTemp(Alert):
     @override
     def is_in_alarm(self) -> bool:
         measurement = self._get_latest("oil_temp")
         # We have the reading, check if it is > 1K
-        if "reading" in measurement.keys() and measurement["reading"] < 35.0:
+        if "reading" in measurement.keys() and measurement["reading"] < 38.0:
             return False
         self.activate()
         return True  # Default to alarm state (also if no data exists)
 
     @override
     def try_enable(self):
-        self.enable_if_below_threshold("oil_temp", 30.0)
+        self.enable_if_below_threshold("oil_temp", 35.0)
 
     @override
     @property
     def description(self) -> str:
-        return "Compressor oil temperature is above 35C. The compressor will fail soon and action must be taken now. Alarm disabled until oil temperature is below 30C."
+        return "Compressor oil temperature is above 38C. The compressor will likely fail soon (at 50C) and action must be taken now. The compressor will then re-enable at 40C. Alarm disabled until oil temperature is below 35C."
 
     @override
     @property
     def title(self) -> str:
-        return "Compressor oil temperature exceeds 35C"
+        return "Compressor oil temperature exceeds 38C"

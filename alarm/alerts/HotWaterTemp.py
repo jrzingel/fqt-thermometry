485cecba9f86903704f86bf895ee44d029b203b9
jrzingel <j.zingel@unsw.edu.au>
2025-06-12 14:55:14 +1000

    Lowered hot water alarm to 20C

--- alarm/alerts/HotWaterTemp.py
+++ alarm/alerts/HotWaterTemp.py
@@ -12,7 +12,7 @@ If it does it isn't out fault :|
 from .Alert import Alert
 
 
-# NORMAL < 35 < WARNING < 38 < ALARM
+# Enabled < 19,  20 < ALARM
 
 class HotWaterTemp(Alert):
     def update_data(self):
@@ -23,22 +23,22 @@ class HotWaterTemp(Alert):
     def is_in_alarm(self) -> bool:
         # We have the reading, check if it is > 38C
         if "reading" in self.data["water_temp"].keys():
-            return self.data["water_temp"]["reading"] > 38.0
+            return self.data["water_temp"]["reading"] > 20.0
         return True  # Default to alarm state (also if no data exists)
 
     def should_enable(self):
-        return self.enable_if_below_threshold("water_temp", 35.0)
+        return self.enable_if_below_threshold("water_temp", 19.0)
 
     @property
     def description(self) -> str:
-        return "Compressor input water temperature is above 38C. The roof chiller has likely failed and so the compressor will too if it is on. Alarm disabled until input water temperature is below 35C."
+        return "Compressor input water temperature is above 20C. The roof chiller has likely failed and so the compressor will too if it is on. Alarm disabled until input water temperature is below 19C."
 
     @property
     def describe_condition(self) -> str:
         if "reading" in self.data["water_temp"].keys():
-            return f"Input water temp = {self.data['water_temp']['reading']:.1f} C | Alarm > 38C, 35C > Enabled"
+            return f"Input water temp = {self.data['water_temp']['reading']:.1f} C | Alarm > 20C, 19C > Enabled"
         return ""
 
     @property
     def title(self) -> str:
-        return "Input water temperature exceeds 38C"
+        return "Input compressor water temperature exceeds 20C"

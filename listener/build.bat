9b4b96b280185117408f8f0a500fab42dd7f3549
James Zingel <j.zingel@unsw.edu.au>
2025-07-08 13:52:38 +1000

    Move completed .exe to the same dir

--- listener/build.bat
+++ listener/build.bat
@@ -9,4 +9,9 @@ call activate thermometry-3-8
 
 pyinstaller.exe -c -F -i app_icon.ico listener.py
 
+move /Y ".\dist\*.exe" ".\"
+rmdir /s /q build
+rmdir /s /q dist
+del listener.spec
+
 call conda deactivate

96fdfee1393d1bfacb59a63a06bf82b9bb09334f
James Zingel <j.zingel@unsw.edu.au>
2025-07-23 16:50:17 +1000

    Updated losing vacuum alert to be the average of 4 readings

--- alarm/alerts/LosingVacuum.py
+++ alarm/alerts/LosingVacuum.py
@@ -8,14 +8,16 @@ Alert for when the vacuum can pressure is lost
 """
 
 from .Alert import Alert
+import numpy as np
 
 # From running this script it appears that sometimes there is a small pressure fluctuation
-# so you really need it to be greater than threshold for two consecutive measurements
+# so you really need it to be greater than threshold for n consecutive measurements (on average)
 
 class LosingVacuum(Alert):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.pre_alarm = False  # Used if the last state was alarm too
+        self.n = 4  # Number of points to average over
+        self.history = [np.nan] * self.n
 
     def update_data(self) -> dict:
         return {
@@ -25,20 +27,20 @@ class LosingVacuum(Alert):
     def is_in_alarm(self) -> bool:
         # check if it is > 1e-5
         if "reading" in self.data["P1"].keys():
-            if self.data["P1"]["reading"] > 10.0e-6:
-                if self.pre_alarm:  # Meaning that the last check was triggered too
-                    return True
-                else:
-                    self.pre_alarm = True
-                    return False
-            else:
-                self.pre_alarm = False  # No longer in previous state of alarm
+            # Add reading to array and shift them over
+            self.history.pop(0)
+            self.history.append(self.data["P1"]["reading"])
+
+            avg = sum(self.history) / self.n
+
+            if np.isnan(avg):
+                # It hasn't been n minutes yet
                 return False
-        return True  # Default to being in alarm
+            return avg > 10.0e-6
+        return False
 
     def should_enable(self) -> bool:
         if self.if_below_threshold("P1", 8.0e-6):
-            self.pre_alarm = False
             return True
         return False
 
